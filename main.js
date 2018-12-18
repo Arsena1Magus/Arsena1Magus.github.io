8793f071788b79c555bc0210694468dd7967b5e5
Nick <arsenalmagus7@gmail.com>
2018-12-18 22:25:30 +0400

    C

--- main.js
+++ main.js
@@ -141,11 +141,11 @@ document.addEventListener("DOMContentLoaded", function ()
 		for (i=i; i < localStorage.length; i++)
 		{
 			j=i+1;
-			if(j < localStorage.length) {
+			if(j <= localStorage.length) {
 			item = JSON.parse(localStorage.getItem('todo'+ [j]));
 			localStorage.setItem('todo'+ [i], JSON.stringify(item));
 		}
-		//	else localStorage.removeItem('todo'+[j]);
+			else localStorage.removeItem('todo'+[j]);
 		}
 	}
 
