698dc9c2f7116c80e93740f486b0d86660596593
Arsena1Magus <34106344+Arsena1Magus@users.noreply.github.com>
2019-02-12 13:47:26 +0300

    C

--- main.js
+++ main.js
@@ -314,7 +314,7 @@ document.addEventListener("DOMContentLoaded", function ()
 				addNewTodoField.blur();
 				input.focus();
 				input.value = text.innerHTML;
-				input.addEventListener('mouseup', function (e)
+				input.addEventListener('keydown', function (e)
 				{
 					if (e.keyCode === 13)
 					{
