e99000e05c8f3fc7f9c0c3d11b456f58800faf6d
Arsena1Magus <34106344+Arsena1Magus@users.noreply.github.com>
2019-02-27 11:09:07 +0300

    C

--- main.js
+++ main.js
@@ -269,10 +269,10 @@ document.addEventListener("DOMContentLoaded", function ()
 				for (var j = 0; j < doneTodos.length; j++)
 					doneTodos[j].style.display = "block";
 			}
-			else (allBtn.classList.contains('selected') == true ){
+			else (allBtn.classList.contains('selected') == true)
 					for (var i = 0; i < todos.length; i++)
 						todos[i].style.display = "block";
-			}
+
 	}
 
 	function display_clear()
