d2cf33b379eaec17b62ace60f638136081cf6f00
Arsena1Magus <34106344+Arsena1Magus@users.noreply.github.com>
2019-03-09 21:48:34 +0300

    С

--- main.js
+++ main.js
@@ -80,11 +80,11 @@ document.addEventListener("DOMContentLoaded", function() {
         item.id = el.todoId;
      var length = el.todoContent.length;
         var quere = length/30;
-        if (quere == 0)
+  /*       if (quere == 0)
           item.style.height = "58px";
           else
         item.style.height = 58*quere + "px";
-      /*     for (;quere!=0; quere--)
+          for (;quere!=0; quere--)
         {
 
         } */
